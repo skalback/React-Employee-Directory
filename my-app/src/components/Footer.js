7537383b9a182fe4e9d3346e3fbc327c25e93bc2
skalback <70301513+skalback@users.noreply.github.com>
2021-02-21 21:26:49 -0500

    Add Footer component

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Footer(){
+    return (
+        <footer>
+            <ul className="footer">
+                <li>Copyright © Seth Kalback</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
